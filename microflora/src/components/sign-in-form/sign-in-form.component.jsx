5210880f127d31c827ae68f2d28b8bc86fb06568
matheusmagenta <matheus.magenta@gmail.com>
2022-06-15 21:36:40 +0100

    created authentication page componenet alongisde sign in form

---
+++ microflora/src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import {
+  createUserDocumentFromAuth,
+  signInWitGooglePopup,
+  signInUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWitGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const clearForm = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInUserWithEmailAndPassword(email, password);
+      // console.log(response);
+      clearForm();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          console.log(error);
+      }
+    }
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>Already have an account</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <div className="buttons-container">
+          <Button type="submit">sign in</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+            google sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
